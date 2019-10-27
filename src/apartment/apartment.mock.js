848266f7e1e41b5a34ff72c38a48281cdf5d9bc0
germiona <violetta.ashirova@gmail.com>
2019-10-27 17:01:03 +0300

    module2-task1

---
+++ src/apartment/apartment.mock.js
@@ -0,0 +1,2 @@
+const cardName = [`Beautiful &amp; luxurious apartment at great location`, `Wood and stone place`, `Canal View Prinsengracht`, `Nice, cozy, warm big bed apartment`, `Wood and stone place`];
+export default cardName;
